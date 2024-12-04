a5ac7645c7bf9266760807670a735c0840fa26ef
micyclebobert <atulpurna@gmail.com>
2024-12-04 13:36:45 +0600

    with music

--- src/main/java/com/rpg/Main.java
+++ src/main/java/com/rpg/Main.java
@@ -1,13 +1,19 @@
 package com.rpg;
 
 import java.awt.Toolkit;
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 public class Main {
 
     public static void main(String[] args) {
         initialize();
-        
+
         GUIMenuScreen.start();
+        playSound();
     }
 
     // Must run first
@@ -18,5 +24,18 @@ public class Main {
     public static void exit(int status) {
         System.out.println("Exit: " + status);
         System.exit(status);
+    }
+
+    public static void playSound() {
+        try {
+            AudioInputStream audioInputStream = AudioSystem
+                    .getAudioInputStream(new File(Helper.getFullPathURL("/music.wav").toURI()).getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
+        }
     }
 }

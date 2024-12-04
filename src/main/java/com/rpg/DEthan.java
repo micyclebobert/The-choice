8307d2415580ac44c78ff574dc1589ac6f99c998
Jobayer <141646914+jobayer1n1@users.noreply.github.com>
2024-12-04 13:13:22 +0600

    Update DEthan.java

--- src/main/java/com/rpg/DEthan.java
+++ src/main/java/com/rpg/DEthan.java
@@ -1,1 +1,32 @@
+package com.rpg;
 
+public class DSofia extends StoryGroup {
+    public static StoryFrame atCrimeScene() {
+        Pair pair1 = new Pair("Search for immediate threats or weapons left behind", e -> updateFrame(atCrimeScene2("You find a knife with dried paint on the handle—possibly the murder weapon.")));
+        Pair pair2 = new Pair("Focus on the cryptic message (What you see is not always real).", e -> updateFrame(atCrimeScene2("You realize the message is a taunt meant to challenge the detectives and delay them.")));
+        return new StoryFrame("The team is at Atul's house : cryptic message,shattered miror, and torn painting\n", pair1, pair2);
+    } 
+    public static StoryFrame atCrimeScene2(String s){
+        Pair pair1 = new Pair("Take the sketchbook to analyze patterns in the drawings", e-> updateFrame(atAbandonedFactory("You identify one recurring face in the face in the sketches a woman who appears to have been watching Atul")));
+        Pair pair2 = new Pair("Question Atul's assistant about the last few days",e-> updateFrame(atAbandonedFactory("The assistant mentins strange letters Atul had been recieving but doesn't know from who")));
+        return new StoryFrame(s+"\nA sketchbook discovered",pair1,pair2);
+    }
+
+    public static StoryFrame atAbandonedFactory(String s){
+        GUIGameScreen.changeBackground(Helper.getImageIcon("/Warehouse.jpg"));
+        Pair pair1 = new Pair("Sneak in quietly and look for clues",e-> updateFrame(theEnd()));
+        Pair pair2 = new Pair(" Confront the masked figure directly.",e->updateFrame(theEnd()));
+        return new StoryFrame(s+"The team is in at the abandoned factory",pair1,pair2);
+    }
+    
+    public static StoryFrame theEnd(){
+        GUIGameScreen.changeBackground(Helper.getImageIcon("/End.jpg"));
+        Pair pair1 = new Pair("Exit", e->Main.exit(0));
+        return new StoryFrame("The end", pair1);
+    }
+    @Override
+    public StoryFrame startingFrame() {
+        return atCrimeScene();
+    }
+        
+}
